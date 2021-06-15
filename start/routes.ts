cf04af7780e7a7c3186df80594b49249b74f5790
Vincent <mail@vincentmoonen.com>
2021-06-15 09:53:29 +0200

    more endpoints

--- start/routes.ts
+++ start/routes.ts
@@ -22,6 +22,8 @@ import Route from '@ioc:Adonis/Core/Route'
 import Video from 'App/Models/Video'
 import FileService from 'App/Services/FileService'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import File from 'App/Models/File'
+import VideoDataPoint from 'App/Models/VideoDataPoint'
 
 Route.get('/', async () => {
   return { hello: 'world' }
@@ -32,12 +34,33 @@ Route.get('/test', async () => {
 })
 
 Route.group(() => {
+
+  // Video stuff
   Route.group(() => {
     Route.group(() => {
       // Get video object
       Route.get('', async ({params}: HttpContextContract) => await Video.find(params.videoId))
       // Get video stream
       Route.get('/stream', 'VideosController.stream')
+      // Get data points
+      Route.get('/datapoints', async ({params}: HttpContextContract) => await VideoDataPoint.query().where('videoId', params.videoId).orderBy('sequenceNumber', 'asc'))
     }).prefix('/:videoId')
   }).prefix('/videos')
+
+  // File stuff
+  Route.group(() => {
+    Route.group(() => {
+      // Get file object
+      Route.get('', async ({params}: HttpContextContract) => await File.find(params.fileId))
+    }).prefix('/:fileId')
+  }).prefix('/files')
+
+  // Datapoints
+  Route.group(() => {
+    Route.group(() => {
+      // Get datapoint object
+      Route.get('', async ({params}: HttpContextContract) => await VideoDataPoint.find(params.datapointId))
+    }).prefix('/:datapointId')
+  }).prefix('/datapoints')
+
 }).prefix('/api')
